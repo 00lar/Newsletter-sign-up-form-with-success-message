fa7f5c70d599b39c8032a2b17dc615d8c82e434c
00lar <eduardani84@gmail.com>
2023-07-07 16:43:06 -0600

    page finished

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 let BtnSubmit = document.querySelector("#BtnSubmit");
+let BtnDismiss = document.querySelector("#BtnDismiss");
 
 const Parte1 = document.querySelector("#Parte1");
 const Parte2 = document.querySelector("#Parte2");
 const correo = document.querySelector("#emailBox");
 const ErrorAlert = document.querySelector("#ErrorAlert");
 const success = document.querySelector("#success")
+let corrspan = document.querySelector("#corrspan");
 
-
+BtnDismiss.addEventListener("click",Cerrar);
+function Cerrar(){
+    location.reload();
+}
 
 BtnSubmit.addEventListener("click",ShowPart3)
 
@@ -20,11 +25,11 @@ function ShowPart3(event){
             Parte1.classList.add("hiddens");
             Parte2.classList.add("hiddens")
             success.classList.remove("hiddens")
+            corrspan.innerHTML = CorreoProp ;
         } 
     }
     else {
-        ErrorAlert.classList.remove("hiddens")
-        
+        ErrorAlert.classList.remove("hiddens") 
     }
 
 
